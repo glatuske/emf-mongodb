a759e0a0008dfd2a660028eeffe84482d1ed9792
Gregor Latuske <Gregor.Latuske@etas.com>
2021-11-11 10:35:29 +0100

    inclue parsing to EMF model

--- src/main/java/net/latuske/emfmongodb/EmfMogodb.java
+++ src/main/java/net/latuske/emfmongodb/EmfMogodb.java
@@ -1,5 +1,6 @@
 package net.latuske.emfmongodb;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Map;
@@ -33,17 +34,17 @@ public class EmfMogodb {
 		collection.findAndRemove(removeQuery);
 
 		Address address1 = MyFactory.eINSTANCE.createAddress();
-		address1.setCity("Kornwestheim");
+		address1.setCity("Stuttgart");
 
 		Person person1 = MyFactory.eINSTANCE.createPerson();
-		person1.setName("Gregor");
+		person1.setName("Alice");
 		person1.getAddresses().add(address1);
 
 		Address address2 = MyFactory.eINSTANCE.createAddress();
-		address2.setCity("Kornwestheim");
+		address2.setCity("Stuttgart");
 
 		Person person2 = MyFactory.eINSTANCE.createPerson();
-		person2.setName("Sabrina");
+		person2.setName("Bob");
 		person2.getAddresses().add(address2);
 
 		long start = System.currentTimeMillis();
@@ -54,37 +55,55 @@ public class EmfMogodb {
 		System.out.println("Insert: " + (System.currentTimeMillis() - start));
 
 		start = System.currentTimeMillis();
-		DBObject query1 = new BasicDBObject("name", "Gregor");
+		DBObject query1 = new BasicDBObject("name", "Alice");
 		DBCursor cursor = collection.find(query1);
-		System.out.println(cursor.one());
+		System.out.println(loadFromDbObject(cursor.one()));
 		System.out.println("Find: " + (System.currentTimeMillis() - start));
 
-		DBObject query2 = new BasicDBObject("name", "Sabrina");
+		DBObject query2 = new BasicDBObject("name", "Bob");
 		cursor = collection.find(query2);
-		System.out.println(cursor.one());
+		System.out.println(loadFromDbObject(cursor.one()));
 
 		DBObject object2 = saveToDbObject(person2);
 		collection.insert(object2);
 
 		cursor = collection.find(query2);
-		System.out.println(cursor.one());
+		System.out.println(loadFromDbObject(cursor.one()));
 
 		collection.findAndRemove(removeQuery);
 		cursor = collection.find(query1);
-		System.out.println(cursor.one());
+		System.out.println(loadFromDbObject(cursor.one()));
 	}
 
 	private static DBObject saveToDbObject(Person person1) throws IOException {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-		ResourceSet resourceSet = new ResourceSetImpl();
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("json", new JsonResourceFactory());
-		Resource resource = resourceSet.createResource(URI.createFileURI("my.json"));
+		Resource resource = createResource();
 		resource.getContents().add(person1);
 		resource.save(outputStream, Map.of());
 
 		String jsonString = outputStream.toString();
 		return (DBObject) JSON.parse(jsonString);
+	}
+
+	private static Person loadFromDbObject(DBObject dbObject) throws IOException {
+		if (dbObject == null) {
+			return null;
+		}
+
+		String jsonString = JSON.serialize(dbObject);
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(jsonString.getBytes());
+
+		Resource resource = createResource();
+		resource.load(inputStream, Map.of());
+
+		return (Person) resource.getContents().get(0);
+	}
+
+	private static Resource createResource() {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("json", new JsonResourceFactory());
+		return resourceSet.createResource(URI.createFileURI("my.json"));
 	}
 
 }
