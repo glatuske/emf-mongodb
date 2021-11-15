c87bc5f3f0fa7c46e7d39e5a0e1aecf6acee366a
Gregor Latuske <Gregor.Latuske@etas.com>
2021-11-15 11:59:54 +0100

    use latest Orient DB API

--- src/main/java/net/latuske/emfmongodb/EmfOrientDb.java
+++ src/main/java/net/latuske/emfmongodb/EmfOrientDb.java
@@ -2,7 +2,6 @@ package net.latuske.emfmongodb;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -18,10 +17,16 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
 
-import com.tinkerpop.blueprints.Direction;
-import com.tinkerpop.blueprints.Edge;
-import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.blueprints.impls.orient.OrientGraph;
+import com.orientechnologies.orient.core.config.OGlobalConfiguration;
+import com.orientechnologies.orient.core.db.ODatabaseSession;
+import com.orientechnologies.orient.core.db.ODatabaseType;
+import com.orientechnologies.orient.core.db.OrientDB;
+import com.orientechnologies.orient.core.db.OrientDBConfig;
+import com.orientechnologies.orient.core.metadata.schema.OClass;
+import com.orientechnologies.orient.core.record.ODirection;
+import com.orientechnologies.orient.core.record.OEdge;
+import com.orientechnologies.orient.core.record.OVertex;
+import com.orientechnologies.orient.core.sql.executor.OResultSet;
 
 import net.latuske.emfmogodb.model.Address;
 import net.latuske.emfmogodb.model.EMailAddress;
@@ -33,8 +38,7 @@ import net.latuske.emfmogodb.model.Person;
 public class EmfOrientDb {
 
 	public static void main(String[] args) throws IOException {
-		Path path = Path.of("orientdb");
-		FileUtils.deleteDirectory(path.toFile());
+		FileUtils.deleteDirectory(Path.of("orientdb").toFile());
 
 		Address address1 = MyFactory.eINSTANCE.createAddress();
 		address1.setCity("Stuttgart");
@@ -65,44 +69,38 @@ public class EmfOrientDb {
 		bob.getEmailAddresses().add(eMailAddress2);
 		bob.getEmailAddresses().add(eMailAddress3);
 
-		OrientGraph graph = new OrientGraph("plocal:orientdb");
-		try {
-			insert(graph, alice);
-			insert(graph, bob);
+		OrientDBConfig config = OrientDBConfig.builder()
+				.addConfig(OGlobalConfiguration.CREATE_DEFAULT_USERS, Boolean.TRUE).build();
 
-			graph.createKeyIndex("emf-package-ns", Vertex.class);
-			graph.createKeyIndex("emf-type", Vertex.class);
+		try (OrientDB orientDB = new OrientDB("embedded:orientdb", config)) {
+			orientDB.createIfNotExists("test", ODatabaseType.PLOCAL);
 
-			MyPackage.eINSTANCE.getEClassifiers().stream().filter(EClass.class::isInstance).map(EClass.class::cast)
-					.map(EClass::getEAllAttributes).flatMap(Collection::stream)
-					.forEach(eAttribute -> graph.createKeyIndex(eAttribute.getName(), Vertex.class));
+			try (ODatabaseSession session = orientDB.open("test", "admin", "admin")) {
+				insert(session, alice);
+				insert(session, bob);
 
-			findByName(graph, "Alice");
-		} finally {
-			graph.shutdown();
+				findByName(session, "Alice");
+				findByNamePattern(session, "%li%");
+			}
 		}
+
 	}
 
-	private static void insert(OrientGraph graph, Person person) {
+	private static void insert(ODatabaseSession session, Person person) {
 		long start = System.currentTimeMillis();
 
-		try {
-			createVertex(graph, person);
-			graph.commit();
-		} catch (Exception e) {
-			graph.rollback();
-		}
+		createVertex(session, person).save();
 
 		System.out.println("Insert took: " + (System.currentTimeMillis() - start));
 		System.out.println();
 	}
 
-	private static Vertex createVertex(OrientGraph graph, EObject eObject) {
+	private static OVertex createVertex(ODatabaseSession session, EObject eObject) {
 		EClass eClass = eObject.eClass();
 
-		Vertex vertex = graph.addVertex(null);
-		vertex.setProperty("emf-package-ns", eClass.getEPackage().getNsURI());
-		vertex.setProperty("emf-type", eClass.getName());
+		OVertex vertex = session.newVertex();
+		vertex.setProperty("emf_package_ns", eClass.getEPackage().getNsURI());
+		vertex.setProperty("emf_type", eClass.getName());
 
 		for (EAttribute eAttribute : eClass.getEAllAttributes()) {
 			Object value = eObject.eGet(eAttribute);
@@ -117,13 +115,14 @@ public class EmfOrientDb {
 		for (EReference eReference : eClass.getEAllContainments()) {
 			Object object = eObject.eGet(eReference);
 
+			OClass oClass = getOrCreateClass(session, eReference);
 			if (eReference.isMany()) {
 				((List<?>) object).stream().map(EObject.class::cast)
-						.map(childEObject -> createVertex(graph, childEObject))
-						.forEach(otherVetex -> vertex.addEdge(eReference.getName(), otherVetex));
+						.map(childEObject -> createVertex(session, childEObject))
+						.forEach(otherVetex -> vertex.addEdge(otherVetex, oClass));
 			} else {
 				if (object instanceof EObject) {
-					vertex.addEdge(eReference.getName(), createVertex(graph, (EObject) object));
+					vertex.addEdge(createVertex(session, (EObject) object), oClass);
 				} else {
 					vertex.setProperty(eReference.getName(), object);
 				}
@@ -133,9 +132,43 @@ public class EmfOrientDb {
 		return vertex;
 	}
 
-	private static EObject createEObject(Vertex vertex) {
-		String emfPackageNs = (String) vertex.getProperty("emf-package-ns");
-		String emfType = (String) vertex.getProperty("emf-type");
+	private static OClass getOrCreateClass(ODatabaseSession session, EReference eReference) {
+		OClass oClass = session.getClass(eReference.getName());
+		if (oClass == null) {
+			oClass = session.createClass(eReference.getName(), "E");
+		}
+
+		return oClass;
+	}
+
+	private static void findByName(ODatabaseSession session, String name) {
+		long start = System.currentTimeMillis();
+		String statement = "SELECT FROM V WHERE " + MyPackage.Literals.PERSON__NAME.getName() + " = ? and emf_type = ?";
+		OResultSet resultSet = session.query(statement, name, MyPackage.Literals.PERSON.getName());
+		System.out.println("Find by name took: " + (System.currentTimeMillis() - start));
+
+		start = System.currentTimeMillis();
+		resultSet.vertexStream().map(EmfOrientDb::createEObject).forEach(System.out::println);
+		System.out.println("Convert took: " + (System.currentTimeMillis() - start));
+		System.out.println();
+	}
+
+	private static void findByNamePattern(ODatabaseSession session, String namePattern) {
+		long start = System.currentTimeMillis();
+		String statement = "SELECT FROM V WHERE " + MyPackage.Literals.PERSON__NAME.getName()
+				+ " like ? and emf_type = ?";
+		OResultSet resultSet = session.query(statement, namePattern, MyPackage.Literals.PERSON.getName());
+		System.out.println("Find by name pattern took: " + (System.currentTimeMillis() - start));
+
+		start = System.currentTimeMillis();
+		resultSet.vertexStream().map(EmfOrientDb::createEObject).forEach(System.out::println);
+		System.out.println("Convert took: " + (System.currentTimeMillis() - start));
+		System.out.println();
+	}
+
+	private static EObject createEObject(OVertex vertex) {
+		String emfPackageNs = (String) vertex.getProperty("emf_package_ns");
+		String emfType = (String) vertex.getProperty("emf_type");
 		EPackage ePackage = EPackage.Registry.INSTANCE.getEPackage(emfPackageNs);
 		EClass eClass = (EClass) ePackage.getEClassifier(emfType);
 		EObject eObject = MyFactory.eINSTANCE.create(eClass);
@@ -166,24 +199,10 @@ public class EmfOrientDb {
 		return eObject;
 	}
 
-	private static List<EObject> createEObjectsForEdges(Vertex vertex, EReference eReference) {
-		Iterable<Edge> edges = vertex.getEdges(Direction.OUT, eReference.getName());
-		return StreamSupport.stream(edges.spliterator(), false).map(edge -> edge.getVertex(Direction.OUT))
+	private static List<EObject> createEObjectsForEdges(OVertex vertex, EReference eReference) {
+		Iterable<OEdge> edges = vertex.getEdges(ODirection.OUT, eReference.getName());
+		return StreamSupport.stream(edges.spliterator(), false).map(edge -> edge.getVertex(ODirection.IN))
 				.map(EmfOrientDb::createEObject).collect(Collectors.toList());
-	}
-
-	private static void findByName(OrientGraph graph, String name) {
-		long start = System.currentTimeMillis();
-		Iterable<Vertex> result = graph.query().has(MyPackage.Literals.PERSON.getName(), name)
-				.has("emf-type", MyPackage.Literals.PERSON.getName()).vertices();
-		System.out.println("Find by name took: " + (System.currentTimeMillis() - start));
-
-		start = System.currentTimeMillis();
-		for (Vertex vertex : result) {
-			System.out.println(createEObject(vertex));
-		}
-
-		System.out.println("Convert took: " + (System.currentTimeMillis() - start));
 	}
 
 }
